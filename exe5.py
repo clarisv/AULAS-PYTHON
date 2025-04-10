2fca5465374061dfcd17a6a0ba044c4986f16e84
Clarice Christine <claricesoarescv@gmail.com>
2025-04-10 14:47:31 -0300

    exe5.py

---
+++ exe5.py
@@ -0,0 +1,13 @@
+
+lista_de_compras = ["Maçã", "Banana", "Leite", "Pão", "Queijo"]
+
+
+lista_de_compras.append("Ovos")
+
+
+lista_de_compras.remove("Banana")
+
+
+print("Lista de Compras:")
+for item in lista_de_compras:
+    print("- " + item)
