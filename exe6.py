e7be68e8d88c6f9ca869134d3d237c1c4296af48
Clarice Christine <claricesoarescv@gmail.com>
2025-04-10 14:54:48 -0300

    exe6.py

---
+++ exe6.py
@@ -0,0 +1,6 @@
+# Definindo uma tupla de coordenadas geográficas
+coordenadas_gps = (40.7128, -74.0060)
+
+print("Coordenadas GPS:")
+print("Latitude:", coordenadas_gps[0])
+print("Longitude:", coordenadas_gps[1])
