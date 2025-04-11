339ad957d9a5b871696db42a8b8447261b882588
Clarice Christine <claricesoarescv@gmail.com>
2025-04-11 20:58:17 -0300

    exe7.py

---
+++ exe7.py
@@ -0,0 +1,12 @@
+## Crie uma lista para cada informação a seguir: lista de números de 1 a 10; Lista com quatro nomes; Lista com o ano que você nasceu e o ano atual.
+
+lista_de_numeros = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
+
+def listar_numeros():
+    print('Lista de números:')
+    
+    for numero in lista_de_numeros:
+        print(f'.{numero}')
+
+listar_numeros()
+
