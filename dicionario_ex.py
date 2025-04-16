faf178c710ccca8dd4bd9c76cd917a64b5e4270b
Clarice Christine <claricesoarescv@gmail.com>
2025-04-16 17:00:05 -0300

    Create dicionario_ex.py

---
+++ dicionario_ex.py
@@ -0,0 +1,8 @@
+pessoa = {
+    'jason': {'username': 'jason', 'idade': '28', 'cidade': 'riacho II', 'profissao': 'atendente'},
+}
+
+if 'jason' in pessoa:
+    print("A chave 'jason' existe no dicionário.")
+else:
+    print("A chave 'jason' não existe no dicionário.")
